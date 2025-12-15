97cb91a45a4b3dec9ac62489050d5d680379ff9e
Кикин Роман Сергеевич <romario.the.real.90@gmail.com>
2025-12-15 19:37:05 +0500

    added screenshots

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from PyQt5.QtWidgets import QApplication
 
 from modules import gui
 
-
 app = QApplication(sys.argv)
 window = gui.MainWindow()
 window.show()
 sys.exit(app.exec_())
-
 
 # TODO 1. "creating" table func (FIX BUGS)
 # TODO 2. exceptions and routers when table is not exist and offer to create it
